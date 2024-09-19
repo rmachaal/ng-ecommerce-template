42574caaf652aed4a4572a72492f63ae0b893555
rmachaal <rahafam@hotmail.co.uk>
2024-09-19 21:27:48 +0100

    Adds comments explaining decorator and class syntax in Angular component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+// Component decorator - adds component metadata to a component class:
 @Component({
-  selector: 'bot-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "bot-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
-  constructor() { }
-  
-  ngOnInit(): void {
-    
-  }
 
+//Define properties and methods to add data, functionality and business logic to component:
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
 }
