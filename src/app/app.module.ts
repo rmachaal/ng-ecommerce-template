8b74f285b09715a38ce971a202e3f316ee7279ba
rmachaal <rahafam@hotmail.co.uk>
2024-09-19 16:09:40 +0100

    Adds new components and updates their files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogueComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
